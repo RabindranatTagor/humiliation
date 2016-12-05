30d5c41ea636f21552fa9fa64daf9138e48b5c80
RabindranatTagor <caicosades@gmail.com>
2016-12-05 19:05:17 +0500

    how to insert that zakaz_num

--- index.php
+++ index.php
@@ -23,20 +23,20 @@ SELECT zakaz.zakaz_name, zakaz_contents.zakaz_contents_quantity*road_signs_catal
     $zakaz_name = ''; //переменная для имени заказа
 
     $result = mysqli_query($link, $query);
-    while($row = mysqli_fetch_array($result, MYSQLI_NUM)){
+    while($row = mysqli_fetch_array($result, MYSQLI_NUM)){ //заюзан массив
         $rows[]=$row;
     }
     mysqli_free_result($result);
 
     foreach($rows as $row){
         echo $row[0]." ".$row[1];
-        $zakaz_name = $row[0]; //принимает первое значение имени
+        $zakaz_name = $row[0]; //принимает значение имени
         $totalcost = $row[1]; //получает стоимость
-        $query = "UPDATE zakaz SET zakaz_sum = zakaz_sum +'$totalcost' WHERE zakaz_name='$zakaz_name'"; //обновляет значение общей суммы заказа 
+        $query = "UPDATE zakaz SET zakaz_sum = zakaz_sum +'$totalcost' WHERE zakaz_name='$zakaz_name'"; //обновляет значение общей суммы заказа. при каждом прогоне ожидаемо удваивает итоговую сумму( впрочем теоретически это дело должно проходить единожды для каждого нового заказа, и блокироваться. а лучше как-то обнуляться, в расчете на то что придется вносить правки. 
         mysqli_query($link, $query);
         mysqli_free_result($result);
     }
-
+    //просто проверка того что получилось
     $query = "SELECT * FROM zakaz ORDER BY idzakaz";
 if ($result = mysqli_query($link, $query)) {
 
