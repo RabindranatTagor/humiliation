69795f59c31e9c041c2ed600f0d0e07960f26d0f
RabindranatTagor <caicosades@gmail.com>
2016-12-23 05:32:32 +0500

    i make stuff

---
+++ customer_processing.php
@@ -0,0 +1,25 @@
+<?php
+    require 'init.php';
+
+    $name = $_POST['custname']; 
+    $inn = $_POST['custinn'];
+    $kpp = $_POST['custkpp'];
+
+    $query = "INSERT INTO customers (name, INN, KPP) VALUES ('$name', '$inn', '$kpp')";
+    $result = mysqli_query($link, $query);
+
+    $redirect = isset($_SERVER['HTTP_REFERER'])? $_SERVER['HTTP_REFERER']:'neworder.php'; 
+    header("Location: $redirect"); 
+    exit();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        <p>FFFUUUUU</p>
+    </body>
+</html>
