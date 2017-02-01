bfc9542582db16b60377efb82d6e897163ae5880
RabindranatTagor <caicosades@gmail.com>
2017-02-01 19:36:00 +0500

    changes1

--- api.php
+++ api.php
@@ -2,30 +2,17 @@
     require 'init.php';
 
     include $root.'/header.php';
-    $pname = $_REQUEST['pos-name'];
-    $tname = $_REQUEST['data-type'];
-    $query = "SELECT price FROM '$tname' WHERE name = '$pname'";
-
-    $result = mysqli_query($link, $query);
-    
-    $price = mysqli_fetch_assoc($result);
-    mysqli_free_result($result);
-    mysqli_close($link);
-
-    echo $price[price];
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title></title>
-    </head>
-    <body>
-        
-    </body>
-</html>
-
-<?php
-include $root.'/footer.php';
-?>
+   if ($_REQUEST['pos-name']) {
+       $pname = $_REQUEST['pos-name'];
+       $tname = $_REQUEST['type'];
+      $query = "SELECT price FROM $tname WHERE name = '$pname'";
+       
+       if(!($result = mysqli_query($link, $query))){
+           echo '<br>MySQLi error: '.mysqli_error($link);    
+          } else {
+            $price = mysqli_fetch_assoc($result);
+            mysqli_free_result($result);
+            mysqli_close($link);
+            echo $price['price'];            
+         }                
+    }
