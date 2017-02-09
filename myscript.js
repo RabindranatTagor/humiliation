83db93f63232f9ee355148d11380d35e73e0910a
vkrms <april504@yandex.ru>
2017-02-09 10:23:53 +1000

    clone row on qty change

--- myscript.js
+++ myscript.js
@@ -1,33 +1,39 @@
-$('#posiziya').change(function(e){ 
-    $name = $(this).find('option:selected').text(); 
-    $type = $(this).find('option:selected').data('type'); 
-    // console.log($name, $type); 
-    $.post('api.php',{'pos-name':$name,'type':$type}).then(function(data){ 
-        // console.log(data); 
-        $('#price').val(data); 
-    }) 
-});
-
-$('#qnty').change(function (s) {
-    $quant = $(this).val();
-    $price = $('#price').val();
-    $payload = {}; //the same as new Object;
-    $payload["pos-quantity"] = $quant;
-    $payload["price"] = $price;
-    $.post('api.php', $payload).then(function (res) { //got to send price back to api; lame but will do
-        //console.log(res);
-        $('#sum').val(res);
+$('.input-pos').change(function(e){
+    $name = $(this).find('option:selected').text();
+    $type = $(this).find('option:selected').data('type');
+    $this = $(this); //$(this) = $('.input-pos)', storing it in var required to use it in a wrap velow
+    $.post('api.php',{'pos-name':$name,'type':$type}).then(function(data){
+        $this.parents('tr').find('.input-price').val(data);
     })
 });
 
-//$(document).ready(function () { //you dont really need document-ready wrap when your script is initilized in the footer
+$('.input-qnty').change(function() {
+    $this = $(this);
+    $quant = $this.val();
+    $row = $this.parents('tr');
+    $price = $row.find('.input-price').val();
+    $row.find('.input-sum').val($quant*$price);
+    if ($row.is(':last-child')) { //I love jQuery, it's like plain english
+      $clone = $row.clone();
+      $clone.find('input').val(null);
+      $clone.insertAfter($row);
+    }
+    //DEGENERATION BELOW
+    // $payload = {};
+    // $payload["pos-quantity"] = $quant;
+    // $payload["price"] = $price;
+    // $.post('api.php', $payload).then(function (res) {
+    //     $this.parents('tr').find('.input-sum').val(res);
+    // })
+});
+
+
 $('#demo-form2').submit(function (e) { //use submit() instead of click() and it's bound to the form itself, not the button
     e.preventDefault(); //e is the default browser submit event, it's prevented
     $record = $(this).serializeArray(); //auto collects form data into array;
-    $record.push({type:$('#posiziya').find('option:selected').data('type')});    
+    $record.push({type:$('#posiziya').find('option:selected').data('type')});
     console.log($record);
     $.post('api.php', $record).then(function (res) {
         console.log(res);
     });
 })
-//});
