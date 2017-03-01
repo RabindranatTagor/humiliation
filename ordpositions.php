5b008d61fff6f05d19a15ac1962ae04508d1e94c
vkrms <april504@yandex.ru>
2017-03-01 18:20:45 +1000

    fucking parenthesis

--- ordpositions.php
+++ ordpositions.php
@@ -3,51 +3,56 @@
 
     include $root.'/header.php';
 
+    echo '<br><br>';
     print_r($_POST);
+    echo '<br><br>';
+
     //order info
     $customer = $_POST['cust-info'];
-    $oname = $_POST ['ord-name'];
+    $oname = $_POST['ord-name'];
     $odate = $_POST['ord-date'];
     $sum = $_POST['sum-tot'];
 
     //order input
-    $query = "INSERT INTO zakaz (name, date, customer, sum)VALUES ('$oname', STR_TO_DATE('$odate', '%m/%d/%Y'), '$customer', '$sum'";
-    $result = mysqli_query($link, $query);
-    mysqli_free_result($result);
-    
+    $query = "INSERT INTO zakaz (name, date, customer, sum) VALUES ('$oname', STR_TO_DATE('$odate', '%m/%d/%Y'), '$customer', '$sum')"; //ебучая закрывающая скобка я проебал пол часа или час
+
+    if ($result = mysqli_query($link, $query)) mysqli_free_result($result);
+    else echo '<br>MySQLi error: '.mysqli_error($link);
+
 
     $query = "SELECT * FROM zakaz WHERE name = '$oname'";
-    $result = mysqli_query($link, $query);
     $info = mysqli_fetch_assoc($result);
-    mysqli_free_result($result);
-    
+    if ($result = mysqli_query($link, $query)) mysqli_free_result($result);
+    else echo '<br>:26 MySQLi error: '.mysqli_error($link);
+
     //order_positions info
-    $zid = $info['id'];
-    $pname = $_POST['pos-name[]'];
-    $ptype = $_POST['pos-type[]'];
-    $pqnty = $_POST['pos-quantity[]'];
+    $zid = $info['idzakaz'];
+    $pname = $_POST['pos-name'];
+    $ptype = $_POST['pos-type'];
+    $pqnty = $_POST['pos-quantity'];
 
     for ($i = 0; $i <count($pname); $i++){
         if ($ptype[$i] == "materials"){
             $query = "INSERT INTO zakaz_contents (idzakaza, id_road_signs, id_materials, quantity) VALUES('$zid', NULL, '$pname[$i]', $pqnty[$i])";
-            $result = mysqli_query($link, $query);
-            mysqli_free_result($result);
+            echo '<br>'.$query.'<br>';
+            if ($result = mysqli_query($link, $query)) mysqli_free_result($result);
+            else echo '<br> MySQLi error: '.mysqli_error($link);
         }
         elseif($ptype[$i] == "road_signs_catalog"){
             $query = "INSERT INTO zakaz_contents (idzakaza, id_road_signs, id_materials, quantity) VALUES('$zid', '$pname[$i]', NULL, $pqnty[$i])";
-            $result = mysqli_query($link, $query);
-            mysqli_free_result($result);            
+            if ($result = mysqli_query($link, $query)) mysqli_free_result($result);
+            else echo '<br>:41 MySQLi error: '.mysqli_error($link);
         }
     }
-    
 
-//    $redirect = isset($_SERVER['HTTP_REFERER'])? 'neworder.php':'neworder.php'; 
-//    header("Location: $redirect"); 
+
+//    $redirect = isset($_SERVER['HTTP_REFERER'])? 'neworder.php':'neworder.php';
+//    header("Location: $redirect");
 //    exit();
 
 ?>
 
-ADD NEW BUTTON HERE -> goes to a new form, returns after to this page            
+ADD NEW BUTTON HERE -> goes to a new form, returns after to this page
 SUMBIT BUTTON HERE ->goes to a pdf
 
 <?php
